08b4e1ea5d1b75bc3e87fc0efd3b5aab1e4b066b
VikramRamanathan <vikramramanathan9@gmail.com>
2022-10-07 22:11:35 +0800

    Fixed Item.java's structure, again, by making it into an explicit subclass of Point.java. This allows for generics to work properly, once again.

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,19 +1,18 @@
-public class Item implements Comparable<Item> {
+public class Item extends Point implements Comparable<Item> {
     public String itemName;
     public String address;
-    public Point exactLocation;
     public double price;
 
     public Item(String itemName, String address, Point exactLocation, double price) {
+        super(exactLocation);
         this.itemName = itemName;
         this.address = address;
-        this.exactLocation = exactLocation;
         this.price = price;
     }
 
     public boolean isEqualTo(Item item) {
         return itemName.equals(item.itemName) && address.equals(item.itemName)
-                && exactLocation.isEqualTo(item.exactLocation) && price == item.price;
+                && x == item.x && y == item.y && price == item.price;
     }
 
     public Item minimizeCost(Item item) { // Returns the one with the lower cost, of the two
