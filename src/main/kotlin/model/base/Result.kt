cf877f4c4dbc1a5295629afc1a2ea472f25a5dfd
h1810126 <h1810126@nushigh.edu.sg>
2022-10-15 16:47:09 +0800

    make images work too

--- src/main/kotlin/model/base/Result.kt
+++ src/main/kotlin/model/base/Result.kt
@@ -8,9 +8,15 @@ import java.net.HttpURLConnection
 import java.net.URL
 import javax.imageio.ImageIO
 
-data class Result(val name: String, val price: Double, val location: String) {
-//    Insert api key
-   val key = "pkdoteyJ1IjoibnVsbHN0ZWxsZW5zYXR6IiwiYSI6ImNsOGxmbnM1eTAxNWEzb21lbXQxcDN6ODAifQdotfNfcF1kzz9OSd0gxVMk7Ag".replace("dot",".")
+data class Result(val name: String, val price: Double, val location: String, val point: Point? = null) {
+
+    // Insert api key
+    private val key =
+        "pkdoteyJ1IjoibnVsbHN0ZWxsZW5zYXR6IiwiYSI6ImNsOGxmbnM1eTAxNWEzb21lbXQxcDN6ODAifQdotfNfcF1kzz9OSd0gxVMk7Ag".replace(
+            "dot",
+            "."
+        )
+
     private fun loadNetworkImage(link: String): ImageBitmap {
         val url = URL(link)
         val connection = url.openConnection() as HttpURLConnection
@@ -25,8 +31,11 @@ data class Result(val name: String, val price: Double, val location: String) {
 
         return Image.makeFromEncoded(byteArray).toComposeImageBitmap()
     }
-    fun getImage(long: Double = -122.4241,lat: Double = 37.78): ImageBitmap {
-        return loadNetworkImage("https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${long},${lat},15.25,0,60/400x400?access_token=${key}"
+
+    fun getImage(long: Double = point?.x ?: -122.4241, lat: Double = point?.y ?: 37.78): ImageBitmap {
+        return loadNetworkImage(
+            "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${long},${lat},15.25,0,60/400x400?access_token=${key}"
         )
     }
+
 }
